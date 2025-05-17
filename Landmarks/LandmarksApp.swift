ff6b42e8befc721746c46b362241da2f7e93eb9c
kyoneken <spry.car7536@fastmail.com>
2025-05-17 16:32:17 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by kyoneken on 2025/05/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,16 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by kyoneken on 2025/05/17.
+//
+
+import Testing
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
